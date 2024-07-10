100bb13e1b45e00d5d01d5227c2947eed84c06e7
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-07-10 15:02:29 +0100

    Adding an example pinia store

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,29 +1,32 @@
 <template>
   <main>
     <h1 class="text-blue-500">Hello World</h1>
-    <button class="px-2 py-2 bg-cyan-500 hover:bg-cyan-600" @click="increment">Count is: {{ count }}</button>
+    <button class="px-2 py-2 bg-cyan-500 hover:bg-cyan-600" @click="increment">Count is: {{ counterStore.count }}</button>
+    <h3>{{ counterStore.doubleCount }}</h3>
   </main>
 </template>
 
 <script>
+import { mapStores } from 'pinia';
+import { useCounterStore } from '@/stores/exampleStore'
+
 export default {
+  computed: {
+    // note we are not passing an array, just one store after the other
+    // each store will be accessible as its id + 'Store'
+    ...mapStores(useCounterStore)
+  },
   data() {
     return {
-      count: 1
     }
   },
   methods: {
     increment() {
-      this.count ++
+      this.counterStore.increment()
     }
   },
   // `mounted` is a lifecycle hook which we will explain later
   mounted() {
-    // `this` refers to the component instance.
-    console.log(this.count) // => 1
-
-    // data can be mutated as well
-    this.count = 2
   }
 }
 
