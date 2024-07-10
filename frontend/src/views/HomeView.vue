59d5abc651109ae17b143544ec35bb55f8c6aa2c
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-07-10 07:52:09 +0100

    Remove bloat files

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-blue-500">Hello World</h1>
+    <button class="px-2 py-2 bg-cyan-500 hover:bg-cyan-600" @click="increment">Count is: {{ count }}</button>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    increment() {
+      this.count ++
+    }
+  },
+  // `mounted` is a lifecycle hook which we will explain later
+  mounted() {
+    // `this` refers to the component instance.
+    console.log(this.count) // => 1
+
+    // data can be mutated as well
+    this.count = 2
+  }
+}
+
+</script>
